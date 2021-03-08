815ec7f44ce62c6e65f0bd2f53f91dbb08fa2605
zhangbo <sin19990111@gmail.com>
2021-03-08 19:42:30 +0800

    创建数据处理程序

---
+++ data_processing.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  9 11:18:00 2019
+@author: MOHAMEDR002
+"""
+
+import numpy as np
+from sklearn import preprocessing
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy import interpolate
+import torch
